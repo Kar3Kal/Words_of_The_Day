4629640e9e4e6408423a31e069913eaf7039fd28
Kar3Kal <arosh.r583@gmail.com>
2025-01-17 11:44:42 +1300

    myFirstCommit

---
+++ Days/audio.js
@@ -0,0 +1,17 @@
+const looming_audioUK = new Audio();
+looming_audioUK.src = "../assets/1_looming_uk.mp3";
+
+const looming_audioUS = new Audio();
+looming_audioUS.src = "../assets/1_looming_us.mp3";
+
+const tiff_audioUK = new Audio();
+tiff_audioUK.src = "../assets/2_tiff_uk.mp3";
+
+const tiff_audioUS = new Audio();
+tiff_audioUS.src = "../assets/2_tiff_us.mp3";
+
+const clench_audioUK = new Audio();
+clench_audioUK.src = "../assets/3_clench_uk.mp3";
+
+const clench_audioUS = new Audio();
+clench_audioUS.src = "../assets/3_clench_us.mp3";
